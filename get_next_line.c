90116c827b30ae0489d6f4e873675bde66d4145c
Simon TRUXILLO <struxill@e2r5p6.42.fr>
2018-12-06 21:32:24 +0100

    GNL...

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,92 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 17:22:55 by struxill          #+#    #+#             */
-/*   Updated: 2018/12/02 22:02:36 by struxill         ###   ########.fr       */
+/*   Updated: 2018/12/06 21:31:36 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_buf_line(char *buf)
+{
+	char	*retstr;
+	size_t	i;
+
+	i = 0;
+	if (ft_strchr(buf, '\n') == NULL)
+	{
+		retstr = ft_strnew(ft_strlen(buf));
+		ft_strncpy(retstr, buf, ft_strlen(buf));
+	}
+	else
+	{
+		while (buf[i] != '\n')
+			i++;
+		retstr = ft_strnew(i);
+		ft_strncpy(retstr, buf, i);
+	}
+	return (retstr);
+}
+
+char	*ft_str_m_cat(char *dst, char *src)
+{
+	size_t	maxlen;
+	char	*str;
+
+	if (dst && src)
+	{
+		maxlen = ft_strlen(dst) + ft_strlen(src);
+		if (!(str = (char*)malloc(sizeof(char) * maxlen + 1)))
+			return (NULL);
+		ft_strncpy(str, dst, ft_strlen(dst);
+		ft_strncat(str, src, maxlen);
+		free(dst);
+		free(src);
+	}
+	return (str);
+}
+
+char	*ft_read_line(const int fd)
+{
+	int		ret;
+	char	buf[BUFF_SIZE + 1];
+	int		flag;
+	char	*str;
+
+	flag = 0;
+	while ((ret = read(fd, buf, BUFF_SIZE)) && flag == 0)
+	{
+		if (ft_strchr(buf, '\n') == NULL)
+		{
+		}
+		else
+		{
+			buf[ret] = '\0';
+			ft_lstadd(&list1, ft_lstnew(str, BUFF_SIZE));
+			*line = list1->content;
+			return (retstr);
+		}
+	}
+}
+
 int	get_next_line(const int fd, char **line)
 {
-	int				ret;
-	char			buf[BUFF_SIZE + 1];
-	size_t			len;
+	//	int				ret;
+	//	char			buf[BUFF_SIZE + 1];
 	static t_list	*list1;
+	char			*str;
 
-	len = 0;
 	if (fd == -1)
 	{
 		ft_putstr("Fd = -1, error\n");
 		return (-1);
 	}
 	list1 = ft_lstnew("", 0);
-	if ((ret = read(fd, buf, BUFF_SIZE)))
-	{
-		buf[ret] = '\0';
-		if (ft_strchr(buf, '\n') == NULL;
-			tempstr = 
-		ft_lstadd(&list1, ft_lstnew(buf, BUFF_SIZE));
-		while 
-		*line = list1->content;
-//		ft_putstr(*line);
-	}
-	//	ft_lst_aff(list1);
-	/*
-	   if ((ret = read(fd, buf, BUFF_SIZE)))
-	   {
-	   printf("buf : %s\n", buf);
-	   while (buf[len] != '\n')
-	   len++;
-	   if (!(*line = malloc(sizeof(buf) * len)))
-	   return (0);
-	   } */
+	str = ft_read_line(fd);
+	ft_lstadd(&list1, ft_lstnew(str, BUFF_SIZE));
+	*line = list1->content;
 	if (ret != 0)
 		return (1);
+	free(str);
 	return (0);
 }
